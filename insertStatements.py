752f6dee3453884a3c2ae0114f550920b701d10d
Brandon Burana <bburana@calpoly.edu>
2022-05-24 03:19:06 -0700

    put some stuff in db

--- insertStatements.py
+++ insertStatements.py
@@ -1,10 +1,9 @@
+from numpy import nan, short
 import requests
 import pandas as pd
+import random
 
 mapping = []
-
-
-
 
 def cleanElement(el):
     try:
@@ -19,23 +18,50 @@ print("mapping", mapping)
 
 
 def getSurveyInsert(record):
-    values = ",".join(record)
+    surveyId = record[0]
+    name = str(record[1])
+    shortName = str(record[2])
+    desc = str(record[3])
+    values = f"{surveyId},'{name}','{shortName}','{desc}'"
     insertStatement = f"INSERT INTO Surveys (SurveyID, ShortName, Name, Description) VALUES ({values});"
     return insertStatement
 
 def getUserInsert(record):
-    values = ",".join(record+["None"])
+    id = record[0]
+    name = record[1]
+    email = record[2]
+    num = random.randint(0,1)
+    role = 'user' if num else 'admin'
+    values = f"{id},'{name}','{email}','{role}'"
     insertStatement = f"INSERT INTO Users (AccountID, Name, Email, Role) VALUES ({values});"
     return insertStatement
 
 def getResponseOptionsInsert(record):
-    values = ",".join(record)
+    id = record[0]
+    pos = record[1]
+    resVal = record[2]
+    resPrompt = record[3]
+    comment = 'null' if record[4] == "nan" else record[4]
+    values = f"{id},{pos},{resVal},'{resPrompt}','{comment}'"
     insertStatement = f"INSERT INTO ResponseOptions (SurveyID, Position, ResponseValue, ResponsePrompt, Comment) VALUES ({values});"
     return insertStatement
 
 def getQuestionsInsert(record):
-    values = ",".join(record)
-    insertStatement = f"INSERT INTO MultipleChoiceQuestions (SurveyID, Position, Prompt, Type, Characteristics, Notes) VALUES ({values});"
+    id = record[0]
+    pos = record[1]
+    prompt = record[2]
+    type = record[3]
+    cha = record[4]
+    notes = record[5]
+    if cha == 'nan':
+        values = f"{id},{pos},'{prompt}',{type}"
+        insertStatement = f"INSERT INTO MultipleChoiceQuestions (SurveyID, Position, Prompt, Type) VALUES ({values});"
+    elif notes == 'nan':
+        values = f"{id},{pos},'{prompt}',{type},'{cha}'"
+        insertStatement = f"INSERT INTO MultipleChoiceQuestions (SurveyID, Position, Prompt, Type, Characteristic) VALUES ({values});"
+    else:
+        values = f"{id},{pos},'{prompt}',{type},'{cha}','{notes}'"
+        insertStatement = f"INSERT INTO MultipleChoiceQuestions (SurveyID, Position, Prompt, Type, Characteristic, Notes) VALUES ({values});"
     return insertStatement
 
 def getUREInsert(record):
@@ -58,10 +84,11 @@ def getUREResponses(record):
     caseText = record[0]
     responseId = caseText.strip().split()[-1]
     questionNumber = 1
+    surveyId = 1
     insertStatement = ""
     for answer in record[2:]:
-        values = f"{responseId},{questionNumber},{answer}"
-        insertStatement += f"INSERT INTO Responses (ResponseID, QuestionNo, Answer) VALUES ({values});\n"
+        values = f"{responseId},{surveyId},{questionNumber},{answer}"
+        insertStatement += f"INSERT INTO Responses (ResponseID, SurveyID, QuestionNo, Answer) VALUES ({values});\n"
         questionNumber += 1
     return insertStatement
 
@@ -76,6 +103,20 @@ def getWorkExpInsert(record):
     userId = record[1]
     values = f"{caseNumber},{surveyId},{dateCompleted},{category},{userId}"
     insertStatement = f"INSERT INTO SurveyResponses (SurveyResponseID, SurveyID, DateCompleted, Category, UserID) VALUES ({values});"
+    return insertStatement
+
+def getWorkResponses(record):
+    if record[0].strip() == "Cases" or record[0].strip() == "nan":
+        return None
+    caseText = record[0]
+    responseId = caseText.strip().split()[-1]
+    questionNumber = 1
+    surveyId = 2
+    insertStatement = ""
+    for answer in record[2:]:
+        values = f"{responseId},{surveyId},{questionNumber},{answer}"
+        insertStatement += f"INSERT INTO Responses (ResponseID, SurveyID, QuestionNo, Answer) VALUES ({values});\n"
+        questionNumber += 1
     return insertStatement
 
 def expInsert(record):
@@ -154,17 +195,16 @@ def main():
     despUrl = "https://raw.githubusercontent.com/IshaanKarvir/CSC366-Team-DropTables-Lab5/main/data/Data-v03.xlsx%20-%20Work%20Preferences.csv"
     # printInsertStatements(surveyUrl, getSurveyInsert)
     # printInsertStatements(userUrl, getUserInsert)
-    # printInsertStatements(questionReponseUrl, getResponseOptionsInsert)
     # printInsertStatements(questionsUrl, getQuestionsInsert)
+
+    printInsertStatements(questionReponseUrl, getResponseOptionsInsert)
     # printInsertStatements(ureExperienceUrl, getUREInsert)
     # printInsertStatements(ureExperienceUrl, getUREResponses)
     # printInsertStatements(workExperienceUrl, getWorkExpInsert)
-    # printInsertStatements(workExperienceUrl, getUREResponses)
+    # printInsertStatements(workExperienceUrl, getWorkResponses)
     # printInsertStatements(expUrl, expInsert)
-    printInsertStatements(despUrl, insertDesiredProfiles)
-    printInsertStatements(despUrl, insertDesScores)
-    # printUserInsertStatements()
-    # printResponseInsertStatements()
+    # printInsertStatements(despUrl, insertDesiredProfiles)
+    # printInsertStatements(despUrl, insertDesScores)
 
 
 if __name__ == "__main__":
